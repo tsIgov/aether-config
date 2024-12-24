c5a40e78baf5b6de17e3190b78695dc728bd2033
Tsvetan Igov <ts.igov@outlook.com>
2024-12-24 03:48:28 +0200

    Configured keyring for vscode

--- igov-pc/user/modules/software-development.nix
+++ igov-pc/user/modules/software-development.nix
@@ -22,5 +22,19 @@ in
       git-crypt
       age
     ];
+
+    home.file = {
+		  ".vscode/argv.json".text = ''
+{
+	// Use software rendering instead of hardware accelerated rendering.
+	// This can help in cases where you see rendering issues in VS Code.
+	// "disable-hardware-acceleration": true,
+
+	"enable-crash-reporter": false,
+	"crash-reporter-id": "fff3759e-696d-42e9-9acf-da40604eb18e",
+	"password-store": "gnome-libsecret"
+}
+      '';
+	  };
   };
 }
